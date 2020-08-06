25557a20e190c9be64e07f78d1bdf05bdecbdc49
srikanthpragada <srikanthpragada@gmail.com>
2020-08-06 07:57:09 +0530

    06-AUG

---
+++ demo/assignments/factors.py
@@ -0,0 +1,6 @@
+# Program to print factors of the given number
+
+num = int(input("Enter a number :"))
+for i in range(2, num // 2 + 1):
+    if num % i == 0:
+        print(i)
---
+++ demo/assignments/month_days.py
@@ -0,0 +1,13 @@
+# Display no. of days in the given month
+
+month = int(input("Enter Month [1-12]:"))
+if month == 2:
+    year = int(input("Enter year :"))
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        print(29)
+    else:
+        print(28)
+elif month in [4, 6, 9, 11]:   # month == 4 or month == 6
+    print(30)
+else:
+    print(31)
