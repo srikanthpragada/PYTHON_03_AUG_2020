6fcc9b038ff808cb364115d9c2611b5b32aa9e04
srikanthpragada <srikanthpragada@gmail.com>
2020-09-15 07:58:11 +0530

    15-SEP

--- webdemo/demo/urls.py
+++ webdemo/demo/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from . import views, ajax_views
 
 urlpatterns = [
     path('home/', views.home),
@@ -9,4 +9,6 @@ urlpatterns = [
     path('employees/', views.list_employees),
     path('add/', views.add_employee),
     path('update/', views.update_salary),
+    path('ajax/', ajax_views.ajax_demo),
+    path('datetime/', ajax_views.send_datetime),
 ]
